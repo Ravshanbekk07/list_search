bbd979e7fb158cb4c164dd30e19ae0a093cd000d
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-07 12:48:29 +0500

    cool

--- find01_max.py
+++ find01_max.py
@@ -5,5 +5,15 @@ def find_max(data):
         data: list of numbers
     returns: maximum number in the list
     """
-    return 0
     
+    max  = 0
+    k = 1
+    while k<len(data):
+        if max<data[k]:
+            max = data[k]
+        k+=1
+    return max
+    
+
+v = find_max([1,2,3,4,5])
+print(v)
